acc50f3ad3ff9fdd3d68ad9d7c74d89fa3868d64
Moy Olvera <moi6olvera@gmail.com>
2020-10-07 23:42:33 -0500

    Added common styles

---
+++ src/theme/CommonStyles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const CommonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  flexOne: {
+    flex: 1,
+  },
+});
+
+export default CommonStyles;
